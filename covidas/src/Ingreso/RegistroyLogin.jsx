3fa7b506d73752f1e3d8fd3e95f9f21fc0dbcab8
Alex <aexlopez@gmail.com>
2021-04-19 21:55:22 -0600

    vistafull login/registro

--- covidas/src/Ingreso/RegistroyLogin.jsx
+++ covidas/src/Ingreso/RegistroyLogin.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { IconButton } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
 import "./style.css";
+import imgLogin from "./img/log.svg";
+import imgRegistro from "./img/register.svg";
 
 export class Inicio extends React.Component {
   render() {
@@ -14,6 +16,12 @@ export class Inicio extends React.Component {
 }
 
 export default function FullInicio({ props }) {
+  //variables
+  const [fperfil, setfperfil] = React.useState(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuUEPOKj_QDRKMi_9yr_Z30Hlg43b4BQzcWg&usqp=CAU"
+  );
+
+  //Funciones que permiten cambiar entre login/registro
   const mostrarRegistro = () => {
     const container = document.querySelector(".container");
     container.classList.add("sign-up-mode");
@@ -24,7 +32,6 @@ export default function FullInicio({ props }) {
     //container.classList.add("sign-up-mode");
     container.classList.remove("sign-up-mode");
   };
-
   return (
     <div className="container">
       <div className="forms-container">
@@ -45,27 +52,44 @@ export default function FullInicio({ props }) {
 
             <p className="social-text">
               O ingresa utilizando la camara
-              <IconButton color="primary">
+              <IconButton color="primary" style={{ color: "#17a178" }}>
                 <PhotoCamera />
               </IconButton>
             </p>
           </div>
           <div className="form sign-up-form">
             <h2 className="title">Registro</h2>
+            <div
+              style={{
+                width: 300,
+                height: 200,
+                backgroundImage: "url(" + fperfil + ")",
+                backgroundSize: "cover",
+                display: "flex",
+                flexFlow: "wrap",
+                justifyContent: "center",
+                alignContent: "flex-end",
+              }}
+            >
+              <button className="btn" onClick={() => {}}>
+                Perfil
+              </button>
+              <div style={{ width: "60%" }}></div>
+            </div>
             <div className="input-field">
               <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Usuario" />
             </div>
             <div className="input-field">
               <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
+              <input type="password1" placeholder="Contraseña" />
             </div>
             <div className="input-field">
               <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
+              <input type="password2" placeholder="Confirmar contraseña" />
             </div>
             <button className="btn" onClick={() => {}}>
-              Ingresar
+              Guardar
             </button>
           </div>
         </div>
@@ -74,7 +98,7 @@ export default function FullInicio({ props }) {
       <div className="panels-container">
         <div className="panel left-panel">
           <div className="content">
-            <h3>Aun no posees una cuenta?</h3>
+            <h3>¿Aún no posees una cuenta?</h3>
             <p>Registrate ahora mismo para estar informado</p>
             <button
               className="btn transparent"
@@ -84,11 +108,11 @@ export default function FullInicio({ props }) {
               Registrarse
             </button>
           </div>
-          <img src="img/log.svg" className="image" alt="" />
+          <img src={imgLogin} className="image" alt="" />
         </div>
         <div className="panel right-panel">
           <div className="content">
-            <h3>Ya posees una cuenta ?</h3>
+            <h3>¿Ya posees una cuenta?</h3>
             <p>Utiliza tus credenciales para ingresar</p>
             <button
               className="btn transparent"
@@ -98,7 +122,7 @@ export default function FullInicio({ props }) {
               Ingresar
             </button>
           </div>
-          <img src="img/register.svg" className="image" alt="" />
+          <img src={imgRegistro} className="image" alt="" />
         </div>
       </div>
     </div>
